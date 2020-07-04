aeb27039ae7fa25aa62ec858ba2dd06deaaa8918
Dany Haddad <danyhaddad43@gmail.com>
2020-07-04 13:09:46 -0500

    first draft

--- u_statistics/auto/summary.el
+++ u_statistics/auto/summary.el
@@ -1,9 +1,29 @@
 (TeX-add-style-hook
  "summary"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "10pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "english") ("inputenc" "utf8") ("tocbibind" "nottoc")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
-    "art10"))
+    "art10"
+    "babel"
+    "inputenc"
+    "tocbibind"
+    "amsfonts"
+    "amsmath"
+    "setspace"
+    "hyperref")
+   (LaTeX-add-labels
+    "eq:general-loss"
+    "eq:uni-dev")
+   (LaTeX-add-environments
+    '("Assumption" LaTeX-env-args ["argument"] 0)
+    '("assumption" LaTeX-env-args ["argument"] 0)
+    '("assumption" LaTeX-env-args ["argument"] 1)
+    '("theorem" LaTeX-env-args ["argument"] 0))
+   (LaTeX-add-bibliographies))
  :latex)
 
