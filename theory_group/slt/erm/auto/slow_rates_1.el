03fe7e52c7d06383ddbce788ca2ff9b8f74c0cfd
Dany Haddad <dhaddad@expedia.com>
2020-08-05 13:59:01 -0500

    slow rates v1

---
+++ theory_group/slt/erm/auto/slow_rates_1.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook
+ "slow_rates_1"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "latin1")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "amsmath"
+    "amsfonts"
+    "graphicx"
+    "hyperref"
+    "inputenc")
+   (LaTeX-add-environments
+    '("theorem" LaTeX-env-args ["argument"] 0))
+   (LaTeX-add-bibliographies
+    "../../../u_statistics/summary.bib"))
+ :latex)
+
